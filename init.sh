e86451a9f92775b7da2168b7fddd9bfb53df3d87
Cameron Anderson <Thrillechs@gmail.com>
2022-02-21 19:40:16 -0500

    update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 docker exec -i mysql mysql -uroot -pErryial123 -e "CREATE database fivem;"
 docker exec -i mysql mysql -uroot -pErryial123 fivem < ./database_init.sql
-docker exec -i mysql mysql -uroot -pErryial123 -e "GRANT ALL PRIVILEGES ON fivem.* TO 'user'@'localhost';"
+docker exec -i mysql mysql -uroot -pErryial123 -e "CREATE USER 'user'@'*' IDENTIFIED BY 'Erryial123';"
+docker exec -i mysql mysql -uroot -pErryial123 -e "GRANT ALL PRIVILEGES ON fivem.* TO 'user'@'*';"
 docker exec -i mysql mysql -uroot -pErryial123 -e "FLUSH PRIVILEGES;"
